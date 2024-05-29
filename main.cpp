fe17acb632b1959db486ca327f0c566a5528e09a
morcsanyitamas <mtomi8@gmail.com>
2024-05-29 15:48:02 +0200

    add multiple time input

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::string name;
+    std:: string line;
+
+    do {
+        std::cout << "What is your name? ";
+        std::cin >> name;
+        std::cout << "Hello, " << name << "!" << std::endl;
+    }while (std::getline(std::cin, line));
+
+
     return 0;
 }
